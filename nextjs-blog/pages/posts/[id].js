967dea45b37b9fc1e86942b941061c23b895cdd6
Matt Ferraro <matthewferraro@Matthews-Air.attlocal.net>
2021-07-05 22:55:16 -0400

    specify that the summary image should be large

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -25,39 +25,19 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
-        <meta
-          property="og:title"
-          content={postData.title}
-        />
-        <meta
-          property="twitter:title"
-          content={postData.title}
-        />
         <meta name="twitter:title" content={postData.title} />
-        {/* <meta name="twitter:card" content="summary" /> */}
-        {/* <meta name="twitter:image:src" content={postData.teaserImage}/> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={postData.teaserImage}/>
         <meta name="twitter:site:id" content="@mferraro89" />
         <meta name="twitter:creator:id" content="@mferraro89" />
-        <meta
-          property="og:image"
-          content={postData.teaserImage}
-        />
-        {/* <meta
-          property="og:type"
-          content="article"
-        /> */}
-        <meta
-          name="description"
-          content={postData.teaser}
-        />
-        <meta
-          property="og:description"
-          content={postData.teaser}
-        />
-        <meta
-          property="twitter:description"
-          content={postData.teaser}
-        />
+        <meta property="twitter:title" content={postData.title} />
+        <meta property="twitter:description" content={postData.teaser} />
+
+        <meta property="og:title" content={postData.title} />
+        <meta property="og:image" content={postData.teaserImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:description" content={postData.teaser} />
+        <meta name="description" content={postData.teaser} />
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
