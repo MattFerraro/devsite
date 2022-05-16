1cafa71d3fed8e76012630465aaf03376442fa30
Matt Ferraro <matthewferraro@Matthews-Air.attlocal.net>
2022-05-16 00:09:08 -0400

    Lots of new words on the MRI post :/

---
+++ nextjs-blog/components/mri/dipole.js
@@ -0,0 +1,221 @@
+
+import React, { useEffect, useState, useRef } from 'react'
+import { calculateTorques, getMousePos, findNorth, findSouth, applyTorques, renderDipole, renderFieldVectors, renderStreamlines } from '../dipole_utils'
+
+const width = 614
+const height = 400
+const PI = 3.1415926
+
+
+const Dipole = (props) => {
+  const dipole1Ref = useRef({
+    x: width * 1/3, y: height/2, w: 10, h: 40,
+    theta: PI/2, dtheta:0, moment: 1,
+    strength:1, numLines: 23});
+  const dipole2Ref = useRef({
+    x: width * 2/3, y: height/2, w: 10, h: 40,
+    theta: -PI/4, dtheta:0, moment: 1,
+    strength: 1, numLines: 23});
+    dipole1Ref.current.radius = dipole1Ref.current.h
+    dipole2Ref.current.radius = dipole2Ref.current.h
+  const backgroundRef = useRef({theta: PI/2, strength: 0, numLines: 18})
+  
+  const selectedDipoleRef = useRef(null)
+
+  const [rotation, setRotation] = useState("unset")
+  const rotationRef = useRef(rotation)
+  useEffect(() => {
+    rotationRef.current = rotation
+  }, [rotation])
+
+  const [showFieldLines, setShowFieldLines] = useState(true)
+  const showFieldLinesRef = useRef(showFieldLines)
+  useEffect(() => {
+    showFieldLinesRef.current = showFieldLines
+  }, [showFieldLines])
+
+  const [showFieldVectors, setShowFieldVectors] = useState(false)
+  const showFieldVectorsRef = useRef(showFieldVectors)
+  useEffect(() => {
+    showFieldVectorsRef.current = showFieldVectors
+  }, [showFieldVectors])
+  
+  const canvasRef = useRef(null);
+  const canvasVisibleRef = useRef(false)
+  const requestIdRef = useRef(null);
+
+
+  const renderFrame = () => {
+    const d1 = dipole1Ref.current
+    const d2 = dipole2Ref.current
+    const background = backgroundRef.current
+
+    // console.log("render")
+
+    // Simulate the physics
+    calculateTorques([d1, d2], background)
+    if (rotationRef.current === "driven") {
+      d2.theta += 0.004
+      d2.dtheta = 0
+    } else if (rotationRef.current === "free") {
+      // apply the torques
+      applyTorques([d2])
+    } else if (rotationRef.current === "fixed") {
+      // Do nothing
+    } else if (rotationRef.current === "unset") {
+      // Do nothing
+    }
+
+    // Draw the scene
+    const ctx = canvasRef.current.getContext("2d")
+    ctx.clearRect(0,0, width, height)
+    
+    if (showFieldLinesRef.current) {
+      renderStreamlines(ctx, [d1, d2], background)
+    }
+    if (showFieldVectorsRef.current) {
+      renderFieldVectors(ctx, [d1, d2], background)
+    }
+
+
+    if (selectedDipoleRef.current && selectedDipoleRef.current.dipole === 1) {
+      
+      renderDipole(ctx, d1, true, selectedDipoleRef.current.north)
+    } else {
+      renderDipole(ctx, d1, false, false)
+    }
+    if (selectedDipoleRef.current && selectedDipoleRef.current.dipole === 2) {
+      renderDipole(ctx, d2, true, selectedDipoleRef.current.north)
+      
+    } else {
+      renderDipole(ctx, d2, false, false)
+    }
+
+  };
+
+  const tick = () => {
+    if (!canvasRef.current) return;
+    if (!canvasVisibleRef.current) {
+      console.log("Cancelling v2 animation")
+      return;
+    }
+    renderFrame();
+    requestIdRef.current = requestAnimationFrame(tick);
+  };
+
+  useEffect(() => {
+    requestIdRef.current = requestAnimationFrame(tick);
+    return () => {
+      cancelAnimationFrame(requestIdRef.current);
+    };
+  }, [])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((evt) => {
+      if (!canvasVisibleRef.current && evt[0].isIntersecting) {
+        console.log("restarting v2 animations")
+        requestIdRef.current = requestAnimationFrame(tick);
+      }
+      console.log("intersection observer rot:", rotation)
+      if (evt[0].intersectionRatio === 1 && rotationRef.current === "unset") {
+        console.log("freeing rotation")
+        setRotation("free")
+      }
+      
+      canvasVisibleRef.current = evt[0].isIntersecting
+    }, {rootMargin: "0px", threshold: [.01, 1]});
+    observer.observe(canvasRef.current);
+  }, [])
+
+
+  const mouseDown = (evt) => {
+    const coords = getMousePos(canvasRef.current, evt)
+    // Which of the monopoles is that close to?
+    
+    const dipoles = [dipole1Ref.current, dipole2Ref.current]
+
+    let minDist = 10000
+    let dipole = null
+    let north = null
+    for (let di = 0; di < dipoles.length; di++) {
+      const d = dipoles[di]
+      const [nx, ny] = findNorth(d)
+      const [sx, sy] = findSouth(d)
+      const distNorth = Math.hypot(nx - coords.x, ny - coords.y)
+      const distSouth = Math.hypot(sx - coords.x, sy - coords.y)
+      
+      if (distNorth < minDist) {
+        minDist = distNorth
+        dipole = di + 1
+        north = true
+      }
+      if (distSouth < minDist) {
+        minDist = distSouth
+        dipole = di + 1
+        north = false
+      }
+    }
+
+    if (minDist < 30) {
+      selectedDipoleRef.current = {
+        "dipole": dipole,
+        "north": north
+      }
+      setRotation("fixed")
+    }
+  }
+
+  const mouseUp = (evt) => {
+    if (selectedDipoleRef.current) {
+      selectedDipoleRef.current = null
+      setRotation("free")
+    }
+  } 
+
+  const mouseMove = (evt) => {
+    if (!selectedDipoleRef.current) return
+    const coords = getMousePos(canvasRef.current, evt)
+    
+    if (selectedDipoleRef.current.dipole === 2) {
+      const centerX = dipole2Ref.current.x
+      const centerY = dipole2Ref.current.y
+      let newTheta = Math.atan2(coords.y - centerY, coords.x - centerX)
+      dipole2Ref.current.dtheta = 0
+
+      if (selectedDipoleRef.current.north) {
+        dipole2Ref.current.theta = newTheta
+      } else {
+        dipole2Ref.current.theta = newTheta + PI
+      }
+    }
+
+    if (selectedDipoleRef.current.dipole === 1) {
+      const centerX = dipole1Ref.current.x
+      const centerY = dipole1Ref.current.y
+      let newTheta = Math.atan2(coords.y - centerY, coords.x - centerX)
+      dipole1Ref.current.dtheta = 0
+
+      if (selectedDipoleRef.current.north) {
+        dipole1Ref.current.theta = newTheta
+      } else {
+        dipole1Ref.current.theta = newTheta + PI
+      }
+    }
+  }
+
+  // <input type="checkbox" checked={rotation==="driven"} id="rotate2" name="rotate" value="rotate" onChange={() => setRotation(rotation==="driven"?"free":"driven")}></input>
+  //     <label htmlFor="rotate2"> Autospin </label>
+
+  // <input type="checkbox" checked={showFieldVectors} id="showfieldvectors2" name="showfieldvectors" value="showfieldvectors" onChange={() => setShowFieldVectors(!showFieldVectors)}></input>
+  //     <label htmlFor="showfieldvectors2"> Show Field Indicators </label>
+
+  // <input type="checkbox" checked={showFieldLines} id="showfieldlines2" name="showfieldlines" value="showfieldlines" onChange={() => setShowFieldLines(!showFieldLines)}></input>
+  //     <label htmlFor="showfieldlines2"> Show Field Lines </label>
+
+  return <div>
+      <canvas ref={canvasRef} onMouseMove={mouseMove} onMouseUp={mouseUp} onMouseDown={mouseDown} className="dipole-vis" width={width} height={height} style={{width:width, height: height}}></canvas>
+    
+    </div>
+}
+
+export default Dipole
--- nextjs-blog/components/mri/gyroscope.js
+++ nextjs-blog/components/mri/gyroscope.js
@@ -4,21 +4,34 @@ import { rotateAboutPoint, setupBasicScene } from './webgl_utils'
 import { OBJLoader } from '../threejsutils/objloader'
 import { Vector3 } from 'three'
 import { RGBELoader } from '../../lib/RGBELoader'
+// import { SpriteText2D, textAlign } from 'three-text2d'
+import SpriteText from 'three-spritetext'
 
 const width = 614
 const height = 400
 const PI = 3.1415926
+
+const angularMomentumColor = 0xf08826
+const forceColor = 0x00bf19
+const torqueColor = 0x6500bd
+const radiusColor = 0x0095e6
 
 const Gyroscope = () => {
     const canvasRef = useRef()
     const webGLContext = useRef()
     const sceneContext = useRef()
     const requestIdRef = useRef(null);
-    const [gravity, setGravity] = useState(1)
+    const [gravity, setGravity] = useState(2)
     const gravityRef = useRef(gravity)
     useEffect(() => {
         gravityRef.current = gravity
     }, [gravity])
+
+    const [phi, setPhi] = useState(.2)
+    const phiRef = useRef(phi)
+    useEffect(() => {
+        phiRef.current = phi
+    }, [phi])
 
     useEffect(() => {
         const {scene, camera, renderer} = setupBasicScene(canvasRef.current)
@@ -82,10 +95,37 @@ const Gyroscope = () => {
                 root.position.set(0,0,.065)
                 pivot.add(root)
                 
-                pivot.rotateOnAxis(new Vector3(0, 1, 0), .4)
+                const tiltAngle = 0.5
+                pivot.rotateOnAxis(new Vector3(0, 1, 0), tiltAngle)
 
                 sceneContext.current.pivot = pivot
                 scene.add(pivot);
+
+                const amArrow = new THREE.ArrowHelper( new Vector3(0, 0, 1), new Vector3(0, 0, 0), .16, angularMomentumColor, .01, .007 );
+                pivot.add(amArrow)
+                
+                const torqueArrow2 = new THREE.ArrowHelper( new Vector3(0, 1, 0), new Vector3(0, 0, .16), .03, torqueColor, .01, .007 );
+                pivot.add(torqueArrow2)
+
+                const FArrow2 = new THREE.ArrowHelper( new Vector3(Math.sin(tiltAngle), 0, -Math.cos(tiltAngle)), new Vector3(0, 0, .065), .03, forceColor, .01, .007 * gravityRef.current );
+                pivot.add(FArrow2)
+
+                const rArrow = new THREE.ArrowHelper( new Vector3(Math.cos(tiltAngle), 0, Math.sin(tiltAngle)), new Vector3(0, 0, 0), Math.sin(tiltAngle) * .065, radiusColor, .01, .007 );
+                pivot.add(rArrow)
+
+                // const sprite = new SpriteText2D("SPRITE", { align: textAlign.center,  font: '20px Arial', fillStyle: '#000000' , antialias: true })
+                // scene.add(sprite)
+
+                // const myText = new SpriteText('My text', 10, "black");
+                // scene.add(myText);
+
+                // const geometry = new THREE.FontGeometry("Hello There", {font: font, size: 80})
+
+                // const axesHelper = new THREE.AxesHelper( .2 );
+                // pivot.add(axesHelper)
+
+                sceneContext.current.FArrow = FArrow2
+                sceneContext.current.torqueArrow = torqueArrow2
 
                 objLoader.load('/resources/mri/spinner.obj', (root) => {
 
@@ -122,13 +162,20 @@ const Gyroscope = () => {
                 const spinner = sceneContext.current.spinner
                 spinner.rotateOnAxis(new Vector3(0, 0, 1), .2)
             }
+
+            if (sceneContext.current.FArrow) {
+                sceneContext.current.FArrow.setLength(gravityRef.current * .01, Math.min(.01, gravityRef.current * .01), Math.min(.007, gravityRef.current * .01))
+            }
+            if (sceneContext.current.torqueArrow) {
+                sceneContext.current.torqueArrow.setLength(gravityRef.current * .01, Math.min(.01, gravityRef.current * .01), Math.min(.007, gravityRef.current * .01))
+            }
         }
     }
 
     return <div>
         <div ref={canvasRef}></div>
         <div style={{width:'100%'}}>
-            <input className='slider' type="range" min="0" max="10" step=".01" value={gravity} onChange={(evt) => {setGravity(evt.target.value)}}></input>
+            <input className='slider' type="range" min="0" max="5" step=".01" value={gravity} onChange={(evt) => {setGravity(evt.target.value)}}></input>
         </div>
     </div>
 }
