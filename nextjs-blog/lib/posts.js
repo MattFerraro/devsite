71a1bb62547eacd7a2b9b3f0a0c11a2b92094d13
Matt Ferraro <mferraro89@gmail.com>
2024-03-11 18:01:53 -0500

    add ci-for-cad post

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,9 +1,9 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import imageSize from 'image-size';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import imageSize from "image-size";
 
-const postsDirectory = path.join(process.cwd(), 'posts')
+const postsDirectory = path.join(process.cwd(), "posts");
 
 const allowList = [
   "cnc-router.mdx",
@@ -12,10 +12,11 @@ const allowList = [
   "geometric-algebra.mdx",
   "joule-thomson.mdx",
   "inverse-of-a-circle.mdx",
+  "ci-for-cad.mdx",
   // "mri.mdx",
   // "impulse-response.mdx",
   // "simple-test.mdx"
-]
+];
 
 export function getSortedPostsData() {
   // Get file names under /posts
@@ -23,31 +24,31 @@ export function getSortedPostsData() {
   // This section controls what shows up on the front page
   // const fileNames = fs.readdirSync(postsDirectory)
   const fileNames = allowList;
-  const allPostsData = fileNames.map(fileName => {
+  const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.mdx$/, '')
+    const id = fileName.replace(/\.mdx$/, "");
 
     // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
 
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents)
+    const matterResult = matter(fileContents);
 
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
-    }
-  })
+      ...matterResult.data,
+    };
+  });
   // Sort posts by date
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
-      return 1
+      return 1;
     } else {
-      return -1
+      return -1;
     }
-  })
+  });
 }
 
 export function getAllPostIds() {
@@ -68,49 +69,49 @@ export function getAllPostIds() {
   // This area controls what builds and is hosted, NOT what ends up on the front page
   // To allow EVERYTHING:
   // const fileNames = fs.readdirSync(postsDirectory)
-  const fileNames = allowList
+  const fileNames = allowList;
   // To allow a subset, use allowList
 
-  
-  return fileNames.map(fileName => {
+  return fileNames.map((fileName) => {
     return {
       params: {
-        id: fileName.replace(/\.mdx$/, '')
-      }
-    }
-  })
+        id: fileName.replace(/\.mdx$/, ""),
+      },
+    };
+  });
 }
 
 export async function getPostDataMDX(id) {
-  const fullPath = path.join(postsDirectory, `${id}.mdx`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const matterResult = matter(fileContents)
+  const fullPath = path.join(postsDirectory, `${id}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const matterResult = matter(fileContents);
 
-  const imgDirectory = path.join(process.cwd(), `public/images/${id}`)
+  const imgDirectory = path.join(process.cwd(), `public/images/${id}`);
 
-  const imgDims = {}
+  const imgDims = {};
   if (fs.existsSync(imgDirectory)) {
-    const imgNames = fs.readdirSync(imgDirectory)
+    const imgNames = fs.readdirSync(imgDirectory);
     // imgNames = imgNames.filter(name => !name.endsWith(".wav"))
     for (let img of imgNames) {
       if (img.startsWith(".")) {
-        continue
+        continue;
       }
-      if (img.endsWith(".mp4") || img.endsWith(".wav") || img.endsWith(".mp3")) {
-        continue
+      if (
+        img.endsWith(".mp4") ||
+        img.endsWith(".wav") ||
+        img.endsWith(".mp3")
+      ) {
+        continue;
       }
       try {
-        const dimensions = imageSize(imgDirectory + "/" + img)
-        imgDims[`/images/${id}/${img}`] = dimensions
+        const dimensions = imageSize(imgDirectory + "/" + img);
+        imgDims[`/images/${id}/${img}`] = dimensions;
       } catch (error) {
-        console.log("Could not read " + imgDirectory + "/" + img)
+        console.log("Could not read " + imgDirectory + "/" + img);
       }
-
-      
     }
   }
-  
-  
+
   return {
     id: id,
     rawContent: matterResult.content,
@@ -118,5 +119,5 @@ export async function getPostDataMDX(id) {
     imgDims: imgDims,
     // content: matterResult.content,
     // ...matterResult.data
-  }
+  };
 }
